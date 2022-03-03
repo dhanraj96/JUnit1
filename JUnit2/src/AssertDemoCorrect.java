67466c04bcba208ddd212d5c918a42dd91736f47
Admin <Admin@DESKTOP-7E78KSE>
2022-03-03 17:53:39 +0530

    Assert and Assumption in Java

---
+++ JUnit2/src/AssertDemoCorrect.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class AssertDemoCorrect
+{	@Test
+		public void simple()
+		{
+			int A = 1;
+			int B = 1;
+			assertEquals(A,B);
+		}
+		@Test
+		public void simple1()
+		{
+			int A = 1;
+			int B = 1;
+			assertEquals(A,B);
+		}
+		@Disabled
+		@Test
+		public void simple2()
+		{
+			int A = 1;
+			int B = 1;
+			assertEquals(A,B);
+			
+		}
+		
+
+	}
+
+
+
